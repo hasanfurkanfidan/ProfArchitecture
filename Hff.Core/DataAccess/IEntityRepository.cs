7681a843d02946095540594714db53ca99fccd52
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-12-20 12:36:40 +0300

    basic database functions added

---
+++ Hff.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Hff.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hff.Core.DataAccess
+{
+    public interface IEntityRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        #region[Get]
+        Task<TEntity> Get(Expression<Func<TEntity, bool>> expression);
+        Task<List<TEntity>> GetList(Expression<Func<TEntity, bool>> expression=null);
+        #endregion
+        #region[Crud]
+        Task Add(TEntity entity);
+        Task Update(TEntity entity);
+        Task Delete(TEntity entity);
+        #endregion
+    }
+}
