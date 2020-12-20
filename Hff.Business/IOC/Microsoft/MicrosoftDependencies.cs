2fcf0b837427c71575e3b771e3c4f04d8c062002
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-12-20 15:23:12 +0300

    Added dependency resolvers

---
+++ Hff.Business/IOC/Microsoft/MicrosoftDependencies.cs
@@ -0,0 +1,25 @@
+﻿using Hff.Business.Abstract;
+using Hff.Business.Concrete;
+using Hff.DataAccess.Abstract;
+using Hff.DataAccess.Concrete.EntityFrameworkCore;
+using Hff.DataAccess.Concrete.EntityFrameworkCore.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hff.Business.IOC.Microsoft
+{
+    public static class MicrosoftDependencies
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            //Business
+            services.AddScoped<IProductService, ProductManager>();
+            //DataAccess
+            services.AddScoped<IProductDal, EfProductDal>();
+            services.AddScoped<DbContext, NorthwindContext>();
+        }
+    }
+}
--- Hff.WebApi/Startup.cs
+++ Hff.WebApi/Startup.cs
@@ -1,3 +1,4 @@
+using Hff.Business.IOC.Microsoft;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace Hff.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
