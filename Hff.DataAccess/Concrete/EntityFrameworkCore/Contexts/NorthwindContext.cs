7fd9fefb1a364496ce1f90a602f049cfe29d0e2e
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-12-20 12:45:56 +0300

    Context class added

---
+++ Hff.DataAccess/Concrete/EntityFrameworkCore/Contexts/NorthwindContext.cs
@@ -0,0 +1,17 @@
+﻿using Hff.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hff.DataAccess.Concrete.EntityFrameworkCore.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-3VB3SSC\\SQLEXPRESS01;Database=Northwind;Integrated Security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
