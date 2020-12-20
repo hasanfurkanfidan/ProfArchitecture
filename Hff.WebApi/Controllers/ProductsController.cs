e4ff1e5b9fc4ba784c93b2ac2ac0b0135295fa7a
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-12-20 15:15:50 +0300

    product controller added

---
+++ Hff.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,84 @@
+﻿using Hff.Business.Abstract;
+using Hff.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hff.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            var result = await _productService.GetListAsync();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getlistbycategory")]
+        public async Task<IActionResult> GetListBtCategory(int categoryId)
+        {
+            var result = await _productService.GetListByCategoryAsync(categoryId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpGet("getbyid")]
+        public async Task<IActionResult> GetById(int productId)
+        {
+            var result = await _productService.GetByIdAsync(productId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("add")]
+        public async Task<IActionResult> Add(Product product)
+        {
+            var result = await _productService.AddAsync(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("delete")]
+        public async Task<IActionResult> Delete(Product product)
+        {
+            var result = await _productService.DeleteAsync(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("update")]
+        public async Task<IActionResult> Update(Product product)
+        {
+            var result = await _productService.UpdateAsync(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+    }
+}
